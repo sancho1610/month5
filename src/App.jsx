cdf3b3600f2e1d396158361e7f299227dde320c3
sancho1610 <sanzarzumadilov487@gmail.com>
2025-12-02 19:34:34 +0600

    dz1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Block } from "./components/Block"
+
+
+function App() {
+
+  return (
+    <div className="container">
+     <Block />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Block.jsx
@@ -0,0 +1,9 @@
+export const Block = () => {
+    return (
+    <>
+        <div className="block">
+            <h2>BLOCK</h2>
+        </div>
+    </>
+    )
+}
